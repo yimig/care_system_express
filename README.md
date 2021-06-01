702d6062a2cf1bff2c51180621e85c3e5469493f
yimig <zhangyiming@programmer.net>
2021-06-01 22:45:20 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# 医生助理系统（后端项目）
+
+notice:make sure you have installed Node.js(v15)
+
+run command
+
+```
+npm install
+```
+
+to download all dependencies.
+It may take some minutes.
+After install complete, run
+
+```
+npm run start
+```
+
+to launch the project.
+
+project url will be **http://localhost:3000**
--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testRouter = require('./routes/test');
 var staffLoginRouter=require('./routes/staff_login');
-var getInfoRouter=require('./routes/get_info')
+var getInfoRouter=require('./routes/get_info');
 
 var app = express();
 
